46a62a78a30f3149e41f098a31aa395c0af988dc
ramon diaz-uriarte (at Bufo) <rdiaz02@gmail.com>
2015-01-28 08:33:12 +0100

    added split plot examples

---
+++ example-split-plot-montgomery.R
@@ -0,0 +1,71 @@
+## This was prompted by some emails in R-sig-teaching
+## From: Richard M. Heiberger <rmh@temple.edu>
+## To: Steven Stoline <sstoline@gmail.com>
+## Cc: R-sig-teaching <R-sig-teaching@r-project.org>
+## Subject: Re: [R-sig-teaching] Analyze Nested and Split-Plot Designs with
+##  R
+## Flags: seen, list
+## Date: Thu 08 Jan 2015 19:58:54 CET
+## Maildir: /Gmail/INBOX
+## List: r-sig-teaching.r-project.org
+## https://stat.ethz.ch/mailman/listinfo/r-sig-teaching
+
+
+## I refer to tables in what is the 8th edition. But I only have access to
+## 5th, where all of this is in chapter 13, pp. 561 and ff.
+
+
+## From Heiberger's email
+
+MontEx14.1 <-
+structure(list(Supplier = structure(c(1L, 1L, 1L, 1L, 1L, 1L,
+1L, 1L, 1L, 1L, 1L, 1L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L,
+2L, 2L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L), .Label = c("1",
+"2", "3"), class = "factor"), Batch = structure(c(1L, 1L, 1L,
+2L, 2L, 2L, 3L, 3L, 3L, 4L, 4L, 4L, 1L, 1L, 1L, 2L, 2L, 2L, 3L,
+3L, 3L, 4L, 4L, 4L, 1L, 1L, 1L, 2L, 2L, 2L, 3L, 3L, 3L, 4L, 4L,
+4L), .Label = c("1", "2", "3", "4"), class = "factor"), Purity = c(1L,
+-1L, 0L, -2L, -3L, -4L, -2L, 0L, 1L, 1L, 4L, 0L, 1L, -2L, -3L,
+0L, 4L, 2L, -1L, 0L, -2L, 0L, 3L, 2L, 2L, 4L, 0L, -2L, 0L, 2L,
+1L, -1L, 2L, 3L, 2L, 1L)), .Names = c("Supplier", "Batch", "Purity"
+), row.names = c(NA, -36L), class = "data.frame")
+
+
+
+MontEx14.1$Supplier <- factor(MontEx14.1$Supplier)
+MontEx14.1$Batch <- factor(MontEx14.1$Batch)
+
+MontEx14.1.aov1 <- aov(Purity ~ Supplier/Batch, data=MontEx14.1)
+summary(MontEx14.1.aov1) ## Table 14.4, but note that F for Suppliers is
+                         ## from ratio of Suppliers to Batches (within
+                         ## suppliers). This can be seen from the Expected
+                         ## Mean Square expressions (which correspond to
+                         ## middle column of Tabel 14.1 ---A Fixed, B
+                         ## Random). 
+
+
+
+MontEx14.1.aov2 <- aov(Purity ~ Supplier*Batch, data=MontEx14.1)
+summary(MontEx14.1.aov2) ## Table 14.5, but again F ratio for suppliers is
+                         ## from ratio of Suppliers/SxB interaction Mean
+                         ## Square
+
+MontEx14.1.aov3 <- aov(Purity ~ Supplier + Error(Supplier:Batch),
+data=MontEx14.1)
+summary(MontEx14.1.aov3)
+
+
+
+
+## Sometimes instructive to average the batches: this way, we assess
+## variation among Suppliers relative to the among-Batch within Supplier:
+
+MontAverage <- aggregate(Purity ~ Supplier + Batch,
+                         data = MontEx14.1, mean)
+
+## F is same as Table 14.4 for Supplier. One can also check that the Sums of
+## Squares here are 1/3 of those in Table 14.4 for Supplier, and that the
+## Residuals SS are 1/3 of the SS for Batches.
+
+summary(aov(Purity ~ Supplier, data = MontAverage)) 
+
---
+++ split-plot-interference-hypoxia-Luis-del-Peso.R
@@ -0,0 +1,141 @@
+## A more complicated split-plot based upon a design from Luis del Peso.
+
+
+## I: interfernce
+
+## H, hypoxia
+
+## R1: cultivo
+
+## R2: each pair of two samples from the same cultivo with the same
+## interference; for each value of R2 we have two (sub)samples, and one of
+## them is under hypoxia (H = h) and the other normoxia (H = o)
+
+## In other words, we take one cultivo, split it into two, one of the two
+## gets one level of I and the other the other level. Once grown, we split
+## each of them into two again, and to each one we give on level of H.
+
+
+## some unbalance
+
+set.seed(123)
+(df <- data.frame(
+    R1 = factor(rep(c(1, 2, 3), c(4, 4, 8))),
+    R2 = factor(rep(letters[1:6], c(rep(2, 4), 4, 4))),
+    I = factor(rep(rep(c("i", "n"), c(2, 2)), 4)),
+    H = factor(rep(c("h", "o"), 8)),
+    Y = rnorm(16)
+    )
+ )
+
+## convenient
+df$R3 <- paste0(df$R1, ":", df$R2)
+
+## balanced
+df1 <- df[-(11:14) , ]
+rownames(df1) <- 1:12
+df1
+
+
+#########################################################################
+#########################################################################
+
+######### Only model factor I
+
+## The error terms expand to R1 and R1:I, and we get a three-stratum table
+summary(aov(Y ~ I + Error(R1/I), data = df1))
+## This is identical, but singularity comes from R2 same as R1:I
+summary(aov(Y ~ I + Error(R1/R2), data = df1))
+
+#### Check with lme, and it does since, when 1 df in numerator, F = t^2
+library(lme4)
+## the next two are identical, as they should be
+summary(lmer(Y ~ I + (1|R1) + (1|R1:I), data = df1))
+summary(lmer(Y ~ I + (1|R1) + (1|R2), data = df1))
+
+
+#########################################################################
+## When there is balance, the above are exactly identical to 
+## a paired t-test averaging over the replicates
+
+## Aggregate data by R1 and I and do t-test
+(dfa <- aggregate(Y ~ R1 + I, data = df1, FUN = mean))
+t.test(dfa$Y[dfa$I == "i"], dfa$Y[dfa$I == "n"], paired = TRUE)
+
+## Of course, this is the same of a paired t-test
+anova(lm(Y ~ I + R1, data = dfa))
+
+
+
+## BUT this ain't correct (both are doing the same thing), as we are
+## saying that I and R1 are crossed, which is not true.  Actually, we can
+## see the problem is the denominator term and the dfs, because the Sum sq
+## are OK. Both are same problem. Here I am saying I have four independent
+## measures of R1= 1, two assigned to i and two to n. That is wrong in
+## this case.
+
+summary(aov(Y ~ I + R1, data = df1))
+summary(aov(Y ~ I + Error(R1), data = df1))
+
+
+
+#########################################################################
+#########################################################################
+
+############ Both factors, I and H, no interaction
+
+## Each main effect in a different stratum and main effect of I same as if
+## we had not modeled H, as it should be
+
+summary(aov(Y ~ I + H + Error(R1/I), data = df1))
+
+
+## The test for H is, of course, the same as a paired t-test done correctly:
+t.test(df1$Y[df1$H == "h"], df1$Y[df1$H == "o"], paired = TRUE)
+## which is the same as
+t.test(df1$Y[df1$H == "h"] - df1$Y[df1$H == "o"])
+## (the above works because the h and o levels within a unit alternate and
+## come one after the other)
+
+## Equivalence with lmer not as good anymore, which is not unexpected, and
+## testing with lmer is a delicate thing, etc.
+
+lm1 <- lmer(Y ~ I + H + (1|R1) + (1|R2), data = df1)
+lm2 <- lmer(Y ~ I + (1|R1) + (1|R2), data = df1)
+anova(lm1, lm2)
+
+
+
+
+#########################################################################
+#########################################################################
+
+############ Both factors, I and H, interaction
+
+## Each main effect in a different stratum, and the interaction only in
+## the lower stratum. As we have modeled interaction, looking at main
+## effect of H is questionable.
+
+summary(aov(Y ~ I * H + Error(R1/I), data = df1))
+
+## Next two are the same, but I think the one above is preferable.
+summary(aov(Y ~ I/H + H + Error(R1/I), data = df1))
+summary(aov(Y ~ I/H + H + Error(R1/I/H), data = df1))
+
+
+## (Trying to do paired t-tests logic here is much harder, or impossible)
+
+
+#########################################################################
+#########################################################################
+
+############ Both factors, I and H, interaction, unbalance
+
+
+summary(aov(Y ~ I * H + Error(R1/I), data = df))
+
+## Equivalence with lmer not good, but then all terms are very
+## non-significant
+m3 <- lmer(Y ~ I * H + (1|R1) + (1|R2), data = df)
+m4 <- lmer(Y ~ I +  H + (1|R1) + (1|R2), data = df)
+anova(m3, m4)
