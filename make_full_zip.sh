5b2f824f73631a5bf01fa6dffac426ee1a038ff7
Ramon Diaz-Uriarte (at Coleonyx) <rdiaz02@gmail.com>
2015-05-05 12:35:40 +0200

    minor change to make_full

--- make_full_zip.sh
+++ make_full_zip.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 runKnitr.sh R-basic-stats.Rnw
 
-./make-html.sh R-basic-stats.Rnw
+# I am not sure the html makes much sense 
+# and it is broken somewhere now
+# ./make-html.sh R-basic-stats.Rnw
 
 zip -r Additional_files_R-basic-stats.zip\
  R-basic-stats.R P53.txt MYC.txt BRCA2.txt\
