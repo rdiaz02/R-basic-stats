0c0cd7b6704169d6723a4475d695780ae0056979
ramon diaz-uriarte (at Bufo) <rdiaz02@gmail.com>
2015-12-28 10:31:06 +0100

    update making of files

--- make_full_zip.sh
+++ make_full_zip.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-runKnitr.sh R-basic-stats.Rnw
+runKnitr-2.sh R-basic-stats.Rnw purl
 
 # I am not sure the html makes much sense 
 # and it is broken somewhere now
 # ./make-html.sh R-basic-stats.Rnw
+
+rm Additional_files_R-basic-stats.zip
 
 zip -r Additional_files_R-basic-stats.zip\
  R-basic-stats.R P53.txt MYC.txt BRCA2.txt\
